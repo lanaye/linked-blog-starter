e39877a9d39fdadc7915c61e31d527db90a77002
Svitlana Yevdokimova <svitlana.yevdokimova@mint.ai>
2025-01-14 14:00:22 +0200

    fix

--- components/misc/note-preview.tsx
+++ components/misc/note-preview.tsx
@@ -7,7 +7,7 @@ const NotePreview = ({ title, content }: Props) => {
   return (
     <span className="note-preview block col-span-2 max-w-[400px] bg-stone-800 rounded shadow-sm p-5 cursor-pointer text-lg hover:bg-stone-700 hover:border-transparent">
       <span className="block font-bold leading-snug tracking-tight truncate mb-1">{title}</span>
-      <span className="block font-normal whitespace-pre-line max-h-[150px]">{content}</span>
+      <span className="block descr font-normal whitespace-pre-line max-h-[150px]">{content}</span>
     </span>
   )
 }
